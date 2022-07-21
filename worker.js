d80b7d1411cfe147376eb939a544e89aa8d82df6
nathanclevenger <nateclev@gmail.com>
2022-07-21 05:03:33 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   fetch: (req) => {
     const url = new URL(req.url)
-    url.hostname = 'esb.denoflare.dev'
+    url.hostname = 'drivly.dev'
     console.log(url.toString())
     return fetch(url.toString())
   }
