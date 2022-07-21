d1b2bb4fb1c8a4784aa9bb36a23632aca8c4cf0c
nathanclevenger <nateclev@gmail.com>
2022-07-21 05:07:15 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,3 @@
 export default {
-  fetch: (req) => {
-    const url = new URL(req.url)
-    url.hostname = 'drivly.dev'
-    console.log(url.toString())
-    return fetch(url.toString())
-  }
+  fetch: (req) => fetch('https://workers.cloudflare.com/cf.json', req)
 }
